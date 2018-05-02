e19b9af82712ddc96a0275882fa54531101873d5
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-05-02 18:21:37 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,7 +4,7 @@ function getIt(){
 }
 
 function frameIt(){
-  $('img').on('load', (e) => {$(e.target).addClass('tasty');});
+  $('img').on('click', (e) => {$(e.currentTarget).addClass('tasty');});
 }
 
 function pressIt(){
